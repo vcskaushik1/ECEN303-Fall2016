694b005cf08e4b1617e4694206d1f45085aa289b
John Vetus <jvetus@tamu.edu>
2016-09-09 00:07:27 -0500

    Add files via upload

--- Students/jvetus/Task3/Bernoulli.py
+++ Students/jvetus/Task3/Bernoulli.py
@@ -1,11 +1,16 @@
 import random
 
 Cardinality = 2
+p = 0.25
 NumberTrials = 300000
 
 TrialSequence = []
 for TrialIndex in range(0, NumberTrials):
-    TrialSequence.append(random.randrange(Cardinality))
+    sample = random.random()
+    if sample < 1-p:
+        TrialSequence.append(0)
+    else:
+        TrialSequence.append(1)
 EmpiricalDistribution = []
 for OutcomeIndex in range(0, Cardinality):
     EmpiricalDistribution.append(TrialSequence.count(OutcomeIndex) / float(NumberTrials))

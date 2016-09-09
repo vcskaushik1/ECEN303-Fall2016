60a674794ad017c56c5a589147946b067c2c8a88
Sebastian Pineda <sebastianpineda@users.noreply.github.com>
2016-09-08 22:03:44 -0500

    Add files via upload

---
+++ Students/sebastianpineda/Task3/Bernoulli.py
@@ -0,0 +1,20 @@
+import random
+
+Cardinality = 2
+NumberTrials = 100
+
+# Probability p
+p = 0.25
+
+TrialSequence = []
+for TrialIndex in range(0, NumberTrials):
+	r = random.random()
+	if r <+ p:
+		    TrialSequence.append(1)
+	else:
+		    TrialSequence.append(0)
+
+EmpiricalDistribution = []
+for OutcomeIndex in range(0, Cardinality):
+    EmpiricalDistribution.append(TrialSequence.count(OutcomeIndex) / float(NumberTrials))
+print EmpiricalDistribution

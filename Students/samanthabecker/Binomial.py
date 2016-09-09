5fb69419fd504cec11ebc7e3ed6a612fb28f0fbf
Samantha Becker <samanthabecker@tamu.edu>
2016-09-09 17:00:36 -0500

    Create Binomial.py

---
+++ Students/samanthabecker/Binomial.py
@@ -0,0 +1,18 @@
+import random
+
+NumberTrials = 10000
+p = 0.85
+n = 10
+TrialSequence = []
+
+for TrialIndex in range(0, NumberTrials):
+    cnt = 0
+    for i in range(n):
+    if random.random() < p:
+        cnt+=1
+    TrialSequence.append(cnt)
+
+EmpiricalDistribution = []
+for OutcomeIndex in range(0, n+1):
+    EmpiricalDistribution.append(TrialSequence.count(OutcomeIndex) / float(NumberTrials))
+print EmpiricalDistribution

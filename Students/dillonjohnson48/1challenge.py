a3813624b1d2c240316800ba80cbaaae2d66fb94
Dillon Johnson <dillonjohnson48@tamu.edu>
2016-09-23 15:09:45 -0500

    Add files via upload
    attempt4

--- Students/dillonjohnson48/1challenge.py
+++ Students/dillonjohnson48/1challenge.py
@@ -1,10 +1,10 @@
-__author__ = "Dillon Johnson"
-__NetID__ = "dillonjohnson48"
-__GitHubID__ = "dillonjohnson48"
-__SelfGrade__ = ""
+__author__ = "Juan Gomez"
+__NetID__ = "juanigomez"
+__GitHubID__ = "juanigomez95"
+__SelfGrade__ = "5"
 __Challenge__ = "1"
-__Answer1__ = ""
-__Answer2__ = ""
+__Answer1__ = "1.0"
+__Answer2__ = "6"
 
 """
 Random Signals and Systems
@@ -12,9 +12,10 @@ Course: ECEN 303-502
 Maximum Grade: 5pt
 """
 
-import random
 import math
+import random
 import matplotlib.pyplot as plt
+
 
 
 ParameterP = 0.3
@@ -22,14 +23,16 @@ NumberFlips = 8
 NumberTrials = 100000
 Trials = []
 
-
-def biasedcoinflip(p=0.5):
-    Num = random.random()
-    if (Num < p):   #p bias
-        return 1    #1 is heads
+def biasedcoinflip(p):
+    x = random.random()
+    "p is the probability of getting heads so if p"
+    "x is a random number between 0 and 1"
+    "if x is smaller than p, it will be heads and vice versa"
+    if x < p:
+        outcome=1
     else:
-        return 0    #0 is tails
-
+        outcome=0
+    return outcome
 
 for TrialIndex1 in range(0, NumberTrials):
     Trials.append(biasedcoinflip(ParameterP))
@@ -40,25 +43,21 @@ print 'The average number of ones is {0:.4f}.'.format(TrialAverage)
 SumTrials = []
 
 for TrialIndex2 in range(0, NumberTrials):
-    Sum = 0                                     #reset value to 0
-
-    for Sample in range(0, NumberFlips):        #0 to NumberFlips value in header
-        Sum = Sum + biasedcoinflip(ParameterP)  #Sum is the accumulative sum of past value and current biased flip
-
-    SumTrials.append(Sum)                       #adds sum to the end of the list
-
+    i = 0
+    for x in range(0,NumberFlips):
+        i = i + biasedcoinflip(ParameterP)
+    SumTrials.append(i)
 
 Distribution = []
 for OutcomeIndex1 in range(0, NumberFlips + 1):
     Distribution.append(SumTrials.count(OutcomeIndex1) / (1.0 * NumberTrials))
 
-print (repr(Distribution))
+print repr(Distribution)
 
-SumDistribution = 0
-for i in Distribution:
-    SumDistribution = SumDistribution + i
-print('Sum of Distribution elements is {0:.2f}.'.format(SumDistribution))
-
+Sum= 0
+for y in range(0,NumberFlips):
+    Sum = Sum + Distribution[y]
+    print Sum
 
 OutcomeIndex2 = range(0, NumberFlips + 1)
 num_bins = len(OutcomeIndex2)
@@ -75,14 +74,11 @@ plt.show()
 """
 Describe what happens to the figure as you vary ParameterP from zero to one.
 What is the sum of the elements in Distribtion?
+
+The the distribution will shift from left to right as heads become more prominent.
+The sum of the distriution will eventually reach the number of coin flips.
+
 Place your answer in the __Answer1__ variable at the top of this file.
 What is the most likely outcome for ParameterP = 0.7 and NumberFlips = 8?
 Place your answer in the __Answer2__ variable at the top of this file.
-
-What is the sum of the elements in Distribtion?
-Place your answer in the __Answer1__ variable at the top of this file.
-
-What is the most likely outcome for ParameterP = 0.7 and NumberFlips = 8?
-Place your answer in the __Answer2__ variable at the top of this file.
-
 """

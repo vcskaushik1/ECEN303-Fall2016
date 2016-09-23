dcbc521564c6674afff2e3d00eca363aeb24b747
Rigoberto Rodriguez <rigorodriguez95@tamu.edu>
2016-09-23 16:51:02 -0500

    Update 1challenge.py

--- Students/rigorodriguez95/1challenge.py
+++ Students/rigorodriguez95/1challenge.py
@@ -1,10 +1,10 @@
-__author__ = ""
-__NetID__ = ""
-__GitHubID__ = ""
-__SelfGrade__ = ""
+__author__ = "Rigoberto Rodriguez"
+__NetID__ = "422008292"
+__GitHubID__ = "rigorodriguez95"
+__SelfGrade__ = "5"
 __Challenge__ = "1"
-__Answer1__ = ""
-__Answer2__ = ""
+__Answer1__ = "1"
+__Answer2__ = "6"
 
 """
 Random Signals and Systems
@@ -17,17 +17,19 @@ import math
 import matplotlib.pyplot as plt
 
 
-ParameterP = 0.3
+ParameterP = 0.7
 NumberFlips = 8
 NumberTrials = 100000
 Trials = []
 
 
 def biasedcoinflip(p=0.5):
-    # EDIT
-    # Create method for biased coin flip
-    # Return 1 for heads, with probability p
-    # and 0 for tails
+    if random.random() < p:
+
+    	return 1
+    else:
+
+    	return 0
 
 
 for TrialIndex1 in range(0, NumberTrials):
@@ -39,18 +41,22 @@ print 'The average number of ones is {0:.4f}.'.format(TrialAverage)
 SumTrials = []
 
 for TrialIndex2 in range(0, NumberTrials):
-    # EDIT
-    # Add NumberFlips coin flips for each SumTrials outcome
-    #
+    count = 0
+
+    for i in range(0, NumberFlips):
+        count += biasedcoinflip(ParameterP)
+    SumTrials.append(count)
 
 Distribution = []
 for OutcomeIndex1 in range(0, NumberFlips + 1):
     Distribution.append(SumTrials.count(OutcomeIndex1) / (1.0 * NumberTrials))
 
 print repr(Distribution)
-# EDIT
-# Print the sum of the elements in Distribution
-#
+ElementSum = 0
+
+for i in range(0, len(Distribution)):
+	ElementSum += Distribution[i]
+print "The sum of the elements in Distribution is ", ElementSum
 
 OutcomeIndex2 = range(0, NumberFlips + 1)
 num_bins = len(OutcomeIndex2)
@@ -72,6 +78,4 @@ Place your answer in the __Answer1__ variable at the top of this file.
 
 What is the most likely outcome for ParameterP = 0.7 and NumberFlips = 8?
 Place your answer in the __Answer2__ variable at the top of this file.
-
-
 """

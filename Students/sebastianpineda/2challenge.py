b2cd41c4d57b1ae6885af2cd279419aa5b512cea
Sebastian Pineda <sebastianpineda@users.noreply.github.com>
2016-09-30 01:33:03 -0500

    Update 2challenge.py

--- Students/sebastianpineda/2challenge.py
+++ Students/sebastianpineda/2challenge.py
@@ -13,34 +13,21 @@ Maximum Grade: 5pt
 import random
 import math
 
-
 p = 1.0/3.0    			# Parameter of digital coin
 ParameterA = 1.0/3.0    # Parameter of digital coin A
 ParameterB = 1.0/2.0    # Parameter of digital coin B
 NumberTrials = 100000
 
-
 def biasedcoinflip(p=0.5):
-
-    """
-    This method returns a one with probability p and it returns a zero with
-    probability (1 - p). The default parameter is p=0.5; this can be changed
-    by passing an argument to the method.
-    """
     return math.floor(random.random() + p)
 
-
 def geometricflip(p=0.5):
-    """
-    This method returns a natural number that denotes the number of digital
-    coin flips needed to obtain a one. It relies on method biasedcoinflip().
-    """
     numberflips = 1
     while biasedcoinflip(p) != 1:
         numberflips = numberflips + 1
     return numberflips
 
-
+	
 print "Part 1\n"
 
 Trials = []
@@ -62,6 +49,7 @@ emProbEven = Trials.count(4)/(1.0 * EvenTrials)
 print "The empirical probability that the number of flips is 4 conditional on number of flips being even is {0:.4f}" \
     .format(emProbEven) + "."
 
+	
 print "\nPart 2\n"
 
 Trials2 = []
@@ -72,14 +60,14 @@ for TrialIndex2 in range(0, NumberTrials):
 	FinalB = 0
 	numberflips = 0
 	
-	while(FinalA == FinalB):
+	while FinalA == FinalB :
 		FinalA = biasedcoinflip(ParameterA)
 		FinalB = biasedcoinflip(ParameterB)
 		numberflips = numberflips + 1
 	
 	Trials2.append(numberflips)
 	
-	if(FinalA == 1):
+	if FinalA == 1 :
 		stopOnA = stopOnA + 1
 	else:
 		stopOnB = stopOnB + 1

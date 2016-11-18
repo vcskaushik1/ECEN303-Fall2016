2a91b4561d47c8ce28e09d7ad1db9dfa3e1cbb4f
cobygeller <coby.geller@tamu.edu>
2016-11-18 15:59:31 -0600

    challenge3 submission

--- Students/cobygeller/3challenge.py
+++ Students/cobygeller/3challenge.py
@@ -1,7 +1,7 @@
-__author__ = ""  # EDIT
-__NetID__ = ""  # EDIT
-__GitHubID__ = ""  # EDIT
-__SelfGrade__ = ""  # EDIT
+__author__ = "Dustin Martin"  # EDIT
+__NetID__ = "tobster34"  # EDIT
+__GitHubID__ = "dustinwm1"  # EDIT
+__SelfGrade__ = "5"  # EDIT
 __Challenge__ = "3"
 
 """
@@ -9,12 +9,9 @@ Random Signals and Systems
 Course: ECEN 303-502
 Maximum Grade: 5
 """
-
-
 import random
 import math
 import matplotlib.pyplot as plt
-
 
 def biasedcoinflip(p=0.5):
     """
@@ -37,16 +34,23 @@ def binomialflips(n=1, p=0.5):
     return numberones
 
 
-def poisson(parameterpoisson=10):
-    #
-    # EDIT
-    #
 
+def poisson(parameterpoisson=10):
+    """
+    This method returns a poisson random variable with parameter lambda.
+    The default parameter is lambda=10.
+    """
+    r = parameterpoisson
+    s = 0
+    sum = 0
+    while (sum < random.random()):
+        sum += pow(r,s)*math.exp(-1*r)/math.factorial(s)
+        s = s + 1
+    return s - 1
 
 def experiment(parameterpoisson=10, p=0.5):
     return binomialflips(poisson(parameterpoisson), p)
-    # return poisson(binomialflips(parameterpoisson3, p))
-
+    #return poisson(binomialflips(parameterpoisson, p))
 
 ParameterPoisson = 10
 NumberTrials = 100000
@@ -59,6 +63,7 @@ print sum(TrialSequence)/len(TrialSequence)
 Distribution = []
 for OutcomeIndex1 in range(0, 21):
     Distribution.append(TrialSequence.count(OutcomeIndex1) / (1.0 * NumberTrials))
+
 
 OutcomeIndex2 = range(0, 21)
 num_bins = len(OutcomeIndex2)
@@ -73,11 +78,10 @@ plt.xticks(XticksIndex, OutcomeIndex2)
 plt.show()
 
 # Question 1: What is the mean of experiment()?
-# Answer 1: EDIT
+# Answer 1: The mean is approx. 4
 
 # Question 2: What is the type of experiment()?
-# Answer 2: EDIT
+# Answer 2: A Poisson Random Variable, Lambda = 4
 
 # Question 3: Do the two distributions match?
-# Answer 3: EDIT
-
+# Answer 3: Yes, both of the plots are similar.

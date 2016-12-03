1c8d282595fd0e4af6c27099a88baf707fed5966
Sebastian Pineda <sebastianpineda@users.noreply.github.com>
2016-12-03 02:02:06 -0600

    Update 4challenge.py

--- Students/sebastianpineda/4challenge.py
+++ Students/sebastianpineda/4challenge.py
@@ -1,16 +1,18 @@
 import random
+import cmath
 import math
 import numpy
 import pylab
+import matplotlib.pyplot as plt
 
-__author__ = ""  # EDIT
-__NetID__ = ""  # EDIT
-__GitHubID__ = ""  # EDIT
-__SelfGrade__ = ""  # EDIT
-__Answer1__ = ""
-__Answer2__ = ""
-__Answer3__ = ""
-__Answer4__ = ""
+__author__ = "Sergio Pineda"
+__NetID__ = "123004451"
+__GitHubID__ = "sebastianpineda"
+__SelfGrade__ = "4.5?"  # Parts of this template shouldn't normally compile, and some parts output complex values that can't be plotted.
+__Answer1__ = "Rayleigh"
+__Answer2__ = "1 and 1"
+__Answer3__ = "Rayleigh"
+__Answer4__ = "1 and 1"
 __Challenge__ = "4"
 
 TrialNumber = 10000
@@ -18,44 +20,54 @@ Uvariable = []
 for trial in range(0, TrialNumber):
 	Uvariable.append(random.random())
 
-numBins = #number of evenly sized bins for histogram
+numBins = 100 #number of evenly sized bins for histogram
 plt.hist(Uvariable, numBins, normed=1, facecolor='green', alpha=0.75)
+plt.show()
 
+def g(x):
+	return -1.0*math.log(1.0-x)
 
+Vvariable = []
 #Make sure to define the function 'g'
 for trial in range(0, len(Uvariable)):
 	Vvariable.append(g(Uvariable[trial]))
 
-numBins = #number of evenly sized bins for histogram
+numBins = 100 #number of evenly sized bins for histogram
 plt.hist(Vvariable, numBins, normed=1, facecolor='green', alpha=0.75)
+plt.show()
 
+Wvariable = []
+def h(x):
+	return math.sqrt(-2.0*(math.log(1.0-x)))
 #Make sure to define the function 'h'
 for trial in range(0, len(Uvariable)):
 	Wvariable.append(h(Uvariable[trial]))
 
 
-numBins = #number of evenly sized bins for histogram
+numBins = 100 #number of evenly sized bins for histogram
 plt.hist(Wvariable, numBins, normed=1, facecolor='green', alpha=0.75)
+plt.show()
 
-
-Uknown1 = []
-Uknown2 = []
+Unknown1 = []
+Unknown2 = []
 for trial in range(0, TrialNumber):
     Uvariable1 = random.random()
     Uvariable2 = random.random()
-    Unkown1.append(math.sqrt(- 2 * math.ln(Uvariable1) * math.cos(2 * math.PI * Uvariable2)))
-    Unkown2.append(math.sqrt(- 2 * math.ln(Uvariable1) * math.cos(2 * math.PI * Uvariable2)))
+    Unknown1.append(math.sqrt(abs(- 2 * math.log(Uvariable1) * math.cos(2 * math.pi * Uvariable2))))
+    Unknown2.append(math.sqrt(abs(- 2 * math.log(Uvariable1) * math.cos(2 * math.pi * Uvariable2))))
 
 
-numBins = #number of evenly sized bins for histogram
-plt.hist(Unkown1, numBins, normed=1, facecolor='green', alpha=0.75)
-numBins = #number of evenly sized bins for histogram
-plt.hist(Unkown2, numBins, normed=1, facecolor='green', alpha=0.75)
+numBins = 100 #number of evenly sized bins for histogram
+plt.hist(Unknown1, numBins, normed=1, facecolor='green', alpha=0.75)
+plt.show()
+numBins = 100 #number of evenly sized bins for histogram
+plt.hist(Unknown2, numBins, normed=1, facecolor='green', alpha=0.75)
+plt.show()
 
 
 '''
-1. What is the type of random variable Unkown1?
-2. What is its mean and variance?
-3. What is the type of random variable Unkown2?
-4. What is its mean and variance?
+1. What is the type of random variable Unknown1? Rayleigh
+2. What is its mean and variance? 1 and 1
+3. What is the type of random variable Unknown2? Rayleigh. Both functions are identical?
+4. What is its mean and variance? 1 and 1
 '''

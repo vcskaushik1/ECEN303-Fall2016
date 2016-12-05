229eac2f4589ba086eaf5aa841e691e71c9ea522
cobygeller <coby.geller@tamu.edu>
2016-12-05 12:35:50 -0600

    challenge 4

--- Students/cobygeller/4challenge.py
+++ Students/cobygeller/4challenge.py
@@ -3,14 +3,14 @@ import math
 import numpy
 import pylab
 
-__author__ = ""  # EDIT
-__NetID__ = ""  # EDIT
-__GitHubID__ = ""  # EDIT
-__SelfGrade__ = ""  # EDIT
-__Answer1__ = ""
-__Answer2__ = ""
-__Answer3__ = ""
-__Answer4__ = ""
+__author__ = "Jacob Geller"  # EDIT
+__NetID__ = "coby.geller"  # EDIT
+__GitHubID__ = "cobygeller"  # EDIT
+__SelfGrade__ = ""  # 
+__Answer1__ = "Gaussian"
+__Answer2__ = "mean is 1 and var is 1"
+__Answer3__ = "Gaussian as well"
+__Answer4__ = "mean is 1 and var is 1"
 __Challenge__ = "4"
 
 TrialNumber = 10000
@@ -18,15 +18,18 @@ Uvariable = []
 for trial in range(0, TrialNumber):
 	Uvariable.append(random.random())
 
-numBins = #number of evenly sized bins for histogram
+numBins = 50 #number of evenly sized bins for histogram
 plt.hist(Uvariable, numBins, normed=1, facecolor='green', alpha=0.75)
 
 
 #Make sure to define the function 'g'
+def g(X):
+	return -1*math.log(1-x)#-log(1-X)
+
 for trial in range(0, len(Uvariable)):
 	Vvariable.append(g(Uvariable[trial]))
 
-numBins = #number of evenly sized bins for histogram
+numBins =50 #number of evenly sized bins for histogram
 plt.hist(Vvariable, numBins, normed=1, facecolor='green', alpha=0.75)
 
 #Make sure to define the function 'h'
@@ -34,7 +37,7 @@ for trial in range(0, len(Uvariable)):
 	Wvariable.append(h(Uvariable[trial]))
 
 
-numBins = #number of evenly sized bins for histogram
+numBins =50 #number of evenly sized bins for histogram
 plt.hist(Wvariable, numBins, normed=1, facecolor='green', alpha=0.75)
 
 
@@ -47,9 +50,9 @@ for trial in range(0, TrialNumber):
     Unkown2.append(math.sqrt(- 2 * math.ln(Uvariable1) * math.cos(2 * math.PI * Uvariable2)))
 
 
-numBins = #number of evenly sized bins for histogram
+numBins =50 #number of evenly sized bins for histogram
 plt.hist(Unkown1, numBins, normed=1, facecolor='green', alpha=0.75)
-numBins = #number of evenly sized bins for histogram
+numBins = 50#number of evenly sized bins for histogram
 plt.hist(Unkown2, numBins, normed=1, facecolor='green', alpha=0.75)
 
 

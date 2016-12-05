072b4ab57761f147d85542218e33e07fcc7933bb
Malik A Aldabbagh <alda836@tamu.edu>
2016-12-05 17:43:15 -0600

    Update 4challenge.py

--- Students/Alda836/4challenge.py
+++ Students/Alda836/4challenge.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt  # i had problems with ploting the graphs before
 # __Answer1__ = "gaussaian"
 # __Answer2__ = " 1, 1"
 # __Answer3__ = "gaussian"
-# __Answer4__ = "1, 1"
+# __Answer4__ = "1, 0"
 # __Challenge__ = "4"
 
 
@@ -24,7 +24,7 @@ Uvariable = []
 for trial in range(0, TrialNumber):
     Uvariable.append(random.random())
 
-numBins = 10  # number of evenly sized bins for histogram
+numBins = 100  # number of evenly sized bins for histogram
 
 plt.hist(Uvariable, numBins, normed=1, facecolor='green', alpha=0.75)
 plt.show()
@@ -40,7 +40,7 @@ Vvariable = []  # to stor the values from the function g(u)
 for trial in range(0, len(Uvariable)):
     Vvariable.append(g(Uvariable[trial]))
 
-numBins = 10  # number of evenly sized bins for histogram
+numBins = 100  # number of evenly sized bins for histogram
 plt.hist(Vvariable, numBins, normed=1, facecolor='green', alpha=0.75)
 plt.show()
 
@@ -55,7 +55,7 @@ Wvariable = []  # trore values coming from h(u)
 for trial in range(0, len(Uvariable)):
     Wvariable.append(h(Uvariable[trial]))
 
-numBins = 10  # number of evenly sized bins for histogram
+numBins = 100  # number of evenly sized bins for histogram
 plt.hist(Wvariable, numBins, normed=1, facecolor='green', alpha=0.75)
 plt.show()
 
@@ -64,20 +64,16 @@ Uknown2 = []
 for trial in range(0, TrialNumber):
     Uvariable1 = random.random()
     Uvariable2 = random.random()
-    Unkown1.append(math.sqrt(- 2 * math.ln(Uvariable1) * math.cos(2 * math.PI * Uvariable2)))
-    Unkown2.append(math.sqrt(- 2 * math.ln(Uvariable1) * math.cos(2 * math.PI * Uvariable2)))
+    Uknown1.append(math.sqrt(- 2 * math.log(Uvariable1)) * math.cos(2 * math.pi * Uvariable2))
+    Uknown2.append(math.sqrt(- 2 * math.log(Uvariable1)) * math.cos(2 * math.pi * Uvariable2))
 
-numBins = 10  # number of evenly sized bins for histogram
-plt.hist(Unkown1, numBins, normed=1, facecolor='green', alpha=0.75)
+numBins = 100  # number of evenly sized bins for histogram
+plt.hist(Uknown1, numBins, normed=1, facecolor='green', alpha=0.75)
 plt.show()
 
-numBins = 10  # number of evenly sized bins for histogram
-plt.hist(Unkown2, numBins, normed=1, facecolor='green', alpha=0.75)
+numBins = 100  # number of evenly sized bins for histogram
+plt.hist(Uknown2, numBins, normed=1, facecolor='green', alpha=0.75)
 plt.show()
-
-
-
-
 
 
 

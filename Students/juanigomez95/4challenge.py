13a063c1c50404ae7c9e329f3b269aee2cfa271c
juanigomez95 <juanigomez@tamu.edu>
2016-12-05 16:01:54 -0600

    Update 4challenge.py

--- Students/juanigomez95/4challenge.py
+++ Students/juanigomez95/4challenge.py
@@ -1,13 +1,13 @@
 import random
 import math
-import numpy
+import numpy as np
 import pylab
 import matplotlib.pyplot as plt
 
 __author__ = "Juan Gomez"
 __NetID__ = "juanigomez"
 __GitHubID__ = "juanigomez95"
-__SelfGrade__ = "5"
+__SelfGrade__ = "4"
 __Answer1__ = "Gaussian"
 __Answer2__ = "Mean 1 Variance 1"
 __Answer3__ = "Gaussian"
@@ -49,8 +49,8 @@ Uknown2 = []
 for trial in range(0, TrialNumber):
     Uvariable1 = random.random()
     Uvariable2 = random.random()
-    Uknown1.append(math.sqrt(- 2 * np.log(Uvariable1) * math.cos(2 * math.pi * Uvariable2)))
-    Uknown2.append(math.sqrt(- 2 * np.log(Uvariable1) * math.cos(2 * math.pi * Uvariable2)))
+    Uknown1.append(math.sqrt(abs(- 2 * np.log(Uvariable1) * math.cos(2 * math.pi * Uvariable2))))
+    Uknown2.append(math.sqrt(abs(- 2 * np.log(Uvariable1) * math.cos(2 * math.pi * Uvariable2))))
 
 
 numBins = 100

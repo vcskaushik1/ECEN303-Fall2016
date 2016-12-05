aabfac1fe638c9be074881f5eb817fe837861b3a
Julio Resendez Jr <resendezjulio@tamu.edu>
2016-12-05 14:59:45 -0600

    Update 4challenge.py

--- Students/resendezjulio/4challenge.py
+++ Students/resendezjulio/4challenge.py
@@ -3,26 +3,38 @@ import math
 import numpy
 import pylab
 
-__author__ = ""  # EDIT
-__NetID__ = ""  # EDIT
-__GitHubID__ = ""  # EDIT
-__SelfGrade__ = ""  # EDIT
-__Answer1__ = ""
-__Answer2__ = ""
-__Answer3__ = ""
-__Answer4__ = ""
+__author__ = "Julio Resendez"  
+__NetID__ = "resendezjulio"
+__GitHubID__ = "resendezjulio"  
+__SelfGrade__ = "4.0/5.0" #I believe I got most of this to compile
+__Answer1__ = "Gaussian"
+__Answer2__ = "Mean = 1.0, Variance =1"
+__Answer3__ = "Gaussian again"
+__Answer4__ = "Mean = 1.0, Variance = 1"
 __Challenge__ = "4"
-
+ 
+	
 TrialNumber = 10000
 Uvariable = []
 for trial in range(0, TrialNumber):
 	Uvariable.append(random.random())
 
-numBins = #number of evenly sized bins for histogram
+numBins = 100 # size of bins 
 plt.hist(Uvariable, numBins, normed=1, facecolor='green', alpha=0.75)
 
+plt.show()
+
+
+def g(x):
+return 1.0*math.log(1.0-x)
 
 #Make sure to define the function 'g'
+for trial in range(0, len(Uvariable)):
+	Vvariable.append(g(Uvariable[trial]))
+	
+
+	
+
 for trial in range(0, len(Uvariable)):
 	Vvariable.append(g(Uvariable[trial]))
 
@@ -47,10 +59,13 @@ for trial in range(0, TrialNumber):
     Unkown2.append(math.sqrt(- 2 * math.ln(Uvariable1) * math.cos(2 * math.PI * Uvariable2)))
 
 
-numBins = #number of evenly sized bins for histogram
+numBins = 100
 plt.hist(Unkown1, numBins, normed=1, facecolor='green', alpha=0.75)
-numBins = #number of evenly sized bins for histogram
+plt.show()
+plt.clf()
+numBins = 1000
 plt.hist(Unkown2, numBins, normed=1, facecolor='green', alpha=0.75)
+plt.show()
 
 
 '''

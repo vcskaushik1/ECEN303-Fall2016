d7c0ddc9cfa8633178210b0acaacbbc806d0bf24
Simmi Mani <Simmi Mani>
2016-12-05 01:19:29 -0600

    updating challenge four

--- Students/sma123/4challenge.py
+++ Students/sma123/4challenge.py
@@ -2,15 +2,17 @@ import random
 import math
 import numpy
 import pylab
+import matplotlib.pyplot as plt
+#import statistics
 
-__author__ = ""  # EDIT
-__NetID__ = ""  # EDIT
-__GitHubID__ = ""  # EDIT
-__SelfGrade__ = ""  # EDIT
-__Answer1__ = ""
-__Answer2__ = ""
-__Answer3__ = ""
-__Answer4__ = ""
+__author__ = "Simmi Mani"  # EDIT
+__NetID__ = "simmi.mani123"  # EDIT
+__GitHubID__ = "sma123"  # EDIT
+__SelfGrade__ = "4"  # EDIT
+__Answer1__ = "Gaussian"
+__Answer2__ = "Mean = 1, Variance = 1"
+__Answer3__ = "Gaussian"
+__Answer4__ = "Mean = 1, Variance = 1"
 __Challenge__ = "4"
 
 TrialNumber = 10000
@@ -18,44 +20,55 @@ Uvariable = []
 for trial in range(0, TrialNumber):
 	Uvariable.append(random.random())
 
-numBins = #number of evenly sized bins for histogram
+numBins = 100#number of evenly sized bins for histogram
 plt.hist(Uvariable, numBins, normed=1, facecolor='green', alpha=0.75)
+plt.show()
 
-
+Vvariable =[]
+def g(x):
+    return -1*math.log(1-x)
 #Make sure to define the function 'g'
 for trial in range(0, len(Uvariable)):
 	Vvariable.append(g(Uvariable[trial]))
 
-numBins = #number of evenly sized bins for histogram
+numBins = 100 #number of evenly sized bins for histogram
 plt.hist(Vvariable, numBins, normed=1, facecolor='green', alpha=0.75)
+plt.show()
 
+Wvariable = []
+def h(x):
+    return math.sqrt( -2.0*(math.log(1.0-x)) )
 #Make sure to define the function 'h'
 for trial in range(0, len(Uvariable)):
 	Wvariable.append(h(Uvariable[trial]))
 
 
-numBins = #number of evenly sized bins for histogram
+numBins = 100 #number of evenly sized bins for histogram
 plt.hist(Wvariable, numBins, normed=1, facecolor='green', alpha=0.75)
+plt.show()
 
-
-Uknown1 = []
-Uknown2 = []
+Unkown1 = []
+Unkown2 = []
 for trial in range(0, TrialNumber):
     Uvariable1 = random.random()
     Uvariable2 = random.random()
-    Unkown1.append(math.sqrt(- 2 * math.ln(Uvariable1) * math.cos(2 * math.PI * Uvariable2)))
-    Unkown2.append(math.sqrt(- 2 * math.ln(Uvariable1) * math.cos(2 * math.PI * Uvariable2)))
+    Unkown1.append(math.sqrt(abs(- 2 * math.log(Uvariable1) * math.cos(2 * math.pi * Uvariable2))))
+    Unkown2.append(math.sqrt(abs(- 2 * math.log(Uvariable1) * math.cos(2 * math.pi * Uvariable2))))
 
 
-numBins = #number of evenly sized bins for histogram
+numBins = 100 #number of evenly sized bins for histogram
 plt.hist(Unkown1, numBins, normed=1, facecolor='green', alpha=0.75)
-numBins = #number of evenly sized bins for histogram
-plt.hist(Unkown2, numBins, normed=1, facecolor='green', alpha=0.75)
 
+plt.show()
+
+plt.clf()
+numBins = 100 #number of evenly sized bins for histogram
+plt.hist(Unkown2, numBins, normed=1, facecolor='green', alpha=0.75)
+plt.show()
 
 '''
-1. What is the type of random variable Unkown1?
-2. What is its mean and variance?
-3. What is the type of random variable Unkown2?
-4. What is its mean and variance?
+1. What is the type of random variable Unkown1? Gaussian
+2. What is its mean and variance? Mean = 1, Variance = 1
+3. What is the type of random variable Unkown2? Gaussian
+4. What is its mean and variance? Mean = 1, Variance = 1
 '''

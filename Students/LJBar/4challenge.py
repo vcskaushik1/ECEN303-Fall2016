76fb157ea3a2896b7a376cd534aee0d93c5cb133
Lucky Jay Barut <LJBar@users.noreply.github.com>
2016-12-05 20:45:11 -0600

    Challenge 4 Updated

--- Students/LJBar/4challenge.py
+++ Students/LJBar/4challenge.py
@@ -3,9 +3,9 @@ import math
 import numpy
 import pylab
 
-__author__ = ""  # EDIT
-__NetID__ = ""  # EDIT
-__GitHubID__ = ""  # EDIT
+__author__ = "" Lucky Jay Barut
+__NetID__ = ""  lazerjay95
+__GitHubID__ = ""  LJBar
 __SelfGrade__ = ""  # EDIT
 __Answer1__ = ""
 __Answer2__ = ""
@@ -18,25 +18,30 @@ Uvariable = []
 for trial in range(0, TrialNumber):
 	Uvariable.append(random.random())
 
-numBins = #number of evenly sized bins for histogram
+numBins = 10
 plt.hist(Uvariable, numBins, normed=1, facecolor='green', alpha=0.75)
-
-
-#Make sure to define the function 'g'
+plt.show()
+plt.clear()
+def g(x):
+	return -1.0*math.log(1.0-x)
+Vvariable = []
 for trial in range(0, len(Uvariable)):
 	Vvariable.append(g(Uvariable[trial]))
 
-numBins = #number of evenly sized bins for histogram
+numBins = 100
 plt.hist(Vvariable, numBins, normed=1, facecolor='green', alpha=0.75)
-
-#Make sure to define the function 'h'
+plt.clear()
+plt.show()
+Wvariable = []
+def h(x):
+	return math.sqrt(-2.0*math.log(1.0-x))
 for trial in range(0, len(Uvariable)):
 	Wvariable.append(h(Uvariable[trial]))
 
 
-numBins = #number of evenly sized bins for histogram
+numBins = 100
 plt.hist(Wvariable, numBins, normed=1, facecolor='green', alpha=0.75)
-
+plt.show()
 
 Uknown1 = []
 Uknown2 = []
@@ -47,9 +52,9 @@ for trial in range(0, TrialNumber):
     Unkown2.append(math.sqrt(- 2 * math.ln(Uvariable1) * math.cos(2 * math.PI * Uvariable2)))
 
 
-numBins = #number of evenly sized bins for histogram
+numBins = 100
 plt.hist(Unkown1, numBins, normed=1, facecolor='green', alpha=0.75)
-numBins = #number of evenly sized bins for histogram
+numBins = 100
 plt.hist(Unkown2, numBins, normed=1, facecolor='green', alpha=0.75)
 
 
